fecf8770de36b79b9f9ac646b3ca51702eabab5f
rlaalsrl715 <minkik715@gmail.com>
2022-11-21 22:14:54 +0900

    feat: login

--- index.js
+++ index.js
@@ -2,29 +2,60 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const bodyParser = require('body-parser')
-const { User } = require('./models/user')
+const {User} = require('./models/user')
 const config = require('./config/key')
+const cookieParser = require('cookie-parser')
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
-
+app.use(cookieParser())
 mongoose.connect(config.mongoURI, {
-     useUnifiedTopology: true
+    useUnifiedTopology: true
 }).then(() => console.log('MongoDB connected...'))
     .catch(err => console.log(err))
 const port = 3000
 
 
-
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.post('/register', (req, res) => {
-     const user = new User(req.body)
-     user.save((err, doc) => {
-          if(err) return res.json({success: false, err})
-          return res.status(200).json({success: true, doc})
-     })
+    const user = new User(req.body)
+    user.save((err, doc) => {
+        if (err) return res.json({success: false, err})
+        return res.status(200).json({success: true, doc})
+    })
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.post('/login', (req, res) => {
+    User.findOne({email: req.body.email}, (err, userInfo) => {
+        console.log(123)
+        if (!userInfo) {
+            return res.json({
+                loginSuccess: false,
+                message: "존재하지 않은 이메일입니다."
+            })
+        }
+        console.log(123)
+        userInfo.comparePassword(req.body.password, (err, isMatch) => {
+            console.log(123)
+            if (!isMatch) {
+                return res.js({
+                    loginSuccess: false,
+                    message: "옳지 않은 비밀번호 입니다."
+                })
+            }
+            userInfo.generateToken((err, user) => {
+                if(err) return res.status(400).send(err);
+                res.cookie('token', user.token)
+                    .status(200)
+                    .json({
+                        loginSuccess: true,
+                        user : user
+                    })
+            })
+        })
+        })
+    })
+
+    app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
--- models/user.js
+++ models/user.js
@@ -1,4 +1,9 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const saltRounds = 10
+
+
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -26,10 +31,46 @@ const userSchema = mongoose.Schema({
     token: {
         type: String
     },
-    tokenExp:{
+    tokenExp: {
         type: Number
     }
 })
+
+userSchema.pre('save', function (next) {
+    let user = this;
+    if (user.isModified('password')) {
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err)
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err)
+                user.password = hash
+                console.log(hash)
+                next()
+            })
+        })
+    } else {
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function (password, cb) {
+    bcrypt.compare(password, this.password, function (err, isMatch) {
+        if (err) return cb(err)
+            cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function (cb){
+    console.log(123)
+    const user = this
+    var token = jwt.sign(user._id.toHexString(), 'secrectToken')
+    user.token = token
+
+    user.save(function (err, user){
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
 
 const User = mongoose.model('user', userSchema)
 module.exports = {User}
